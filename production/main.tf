3667d0027bd789d36487269942514e8ff8c575ac
Kaushik Gnanasekar <gnanasekar.ka@northeastern.edu>
2023-02-14 22:36:39 -0500

    added production env

---
+++ production/main.tf
@@ -0,0 +1,15 @@
+// Get availibilty zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "network" {
+  source = "../modules/network"
+
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+  availability_zones   = data.aws_availability_zones.available.names
+}
---
+++ production/variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  description = "AWS Deployment region"
+  default     = "us-east-2"
+}
+
+variable "profile" {
+  description = "AWS cli profile"
+  default     = "production"
+}
+
+variable "environment" {
+  description = "The Deployment environment"
+  default     = "production"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+}
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for public subnets"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  description = "CIDR block for private subnets"
+}
+
+variable "public_route_destination_cidr_block" {
+  description = "The destination cidr for public route"
+  default     = "0.0.0.0/0"
+}
