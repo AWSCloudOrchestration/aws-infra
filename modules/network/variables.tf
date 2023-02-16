4eb30ec0f021c7da11bc7da6de8d8fdf5cefe4fe
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2023-02-16 12:24:33 -0500

    enforce variable types

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,31 +1,35 @@
 variable "environment" {
+  type        = string
   description = "The Deployment environment"
 }
 
 variable "vpc_cidr" {
+  type        = string
   description = "The CIDR block of the vpc"
 }
 
 variable "public_subnets_cidr" {
-  type        = list(any)
+  type        = list(string)
   description = "The CIDR block for the public subnet"
 }
 
 variable "private_subnets_cidr" {
-  type        = list(any)
+  type        = list(string)
   description = "The CIDR block for the private subnet"
 }
 
 variable "region" {
+  type        = string
   description = "The region to launch the bastion host"
 }
 
 variable "availability_zones" {
-  type        = list(any)
+  type        = list(string)
   description = "The az that the resources will be launched"
 }
 
 variable "public_route_destination_cidr_block" {
+  type        = string
   description = "The destination cidr for public route"
   default     = "0.0.0.0/0"
 }
--- production/variables.tf
+++ production/variables.tf
@@ -1,38 +1,44 @@
 variable "region" {
+  type        = string
   description = "AWS Deployment region"
   default     = "us-east-2"
 }
 
 variable "preferred_availability_zones" {
+  type        = list(string)
   description = "AWS availability zones"
   default     = []
 }
 
 variable "profile" {
+  type        = string
   description = "AWS cli profile"
   default     = "production"
 }
 
 variable "environment" {
+  type        = string
   description = "The Deployment environment"
   default     = "production"
 }
 
 variable "vpc_cidr" {
+  type        = string
   description = "VPC CIDR"
 }
 
 variable "public_subnets_cidr" {
-  type        = list(any)
+  type        = list(string)
   description = "CIDR block for public subnets"
 }
 
 variable "private_subnets_cidr" {
-  type        = list(any)
+  type        = list(string)
   description = "CIDR block for private subnets"
 }
 
 variable "public_route_destination_cidr_block" {
+  type        = string
   description = "The destination cidr for public route"
   default     = "0.0.0.0/0"
 }
