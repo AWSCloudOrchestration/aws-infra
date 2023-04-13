e654dc7731f17be9333ce1512125eb423692dcca
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2023-04-13 16:28:23 -0400

    added variables and some cleanup

--- modules/autoscale/main.tf
+++ modules/autoscale/main.tf
@@ -90,7 +90,11 @@ module "ebs_kms" {
   source = "../kms"
 
   kms_description             = "EBS KMS Key"
-  kms_deletion_window_in_days = var.kms_deletion_window_in_days
+  kms_deletion_window_in_days = var.ebs_kms_deletion_window_in_days
+  kms_key_usage               = var.ebs_kms_key_usage
+  kms_enable_key_rotation     = var.ebs_kms_enable_key_rotation
+  kms_is_enabled              = var.ebs_kms_is_enabled
+  kms_multi_region            = var.ebs_kms_multi_region
 }
 
 ## Launch configuration
